5a7ad486043e385961e9369631e1444ee7849217
Vladislava <zaitsevavladislava13@gmail.com>
2023-01-12 11:18:19 +0300

    tasks010-019

---
+++ 013.Numbers/11/app.js
@@ -0,0 +1,21 @@
+// Напишите программу, в которой вычисляется сумма, разность и произведение
+// двух целых чисел, введенных с клавиатуры.
+
+const a = +prompt();
+const b = +prompt();
+
+if (a % 1 === 0) {
+    console.log('целое число', true);
+} else {
+    console.log('дробное число', false);
+}
+
+if (b % 1 === 0) {
+    console.log('целое число', true);
+} else {
+    console.log('дробное число', false);
+}
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
---
+++ 013.Numbers/19/app.js
@@ -0,0 +1,27 @@
+// Даны три числа a, b, c. Напишите программу, которая находит корни квадратного
+// уравнения ax2 + bx + c = 0
+// Если уравнение имеет два корня, то следует вывести их в порядке возрастания.
+
+const a = +prompt();
+const b = +prompt();
+const c = +prompt();
+
+const value = b * b - 4 * a * c;
+
+if (value > 0) {
+    const x1 = (-b - Math.sqrt(value)) / (2 * a);
+    const x2 = (-b + Math.sqrt(value)) / (2 * a);
+
+    if (x1 > x2) {
+        console.log(x2, x1);
+    }
+    else if (x1, x2) {
+        console.log(x1, x2);
+    }
+    else if (value === 0) {
+        console.log(-b / (2 * a));
+    }
+    else if (value < 0) {
+        console.log('нет корней');
+    }
+}
