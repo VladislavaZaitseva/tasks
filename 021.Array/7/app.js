58632e2eed96e041ac5d37ff5404436dff310278
Vladislava <zaitsevavladislava13@gmail.com>
2023-02-05 02:13:46 +0300

    array

---
+++ 021.Array/7/app.js
@@ -0,0 +1,24 @@
+// На входе задан массив [‘hschool’, ‘hschool_0’, ‘hschool_1’]. Напишите все возможные
+// способы обращения к первому и последнему элементам
+
+// let arr = ['hschool', 'hschool_0', 'hschool_1'];
+// let first = arr[0];
+
+// for (i=0; i<arr.length-1; i++);
+// console.log(arr[0]);
+
+// let arr = ['hschool', 'hschool_0', 'hschool_1'];
+// let lastArr = arr[arr.length-1];
+// console.log(lastArr);
+
+
+// let arr = ['hschool', 'hschool_0', 'hschool_1'];
+// let lastArr = arr.slice(-1);
+// console.log(lastArr);
+
+let arr = ['hschool', 'hschool_0', 'hschool_1'];
+let lastArr = arr.pop();
+console.log(lastArr);
+
+
+
---
+++ 021.Array/8/app.js
@@ -0,0 +1,26 @@
+// На входе n – количество элементов массива. Далее производится заполнение
+// массива с клавиатуры. Если все элементы массива – числа, то вывести логическое
+// true, в противном случае false. Every, forEach
+
+const n = 3;
+let arr = [];
+
+for (let i = 0; i < n; i++) {
+    arr.push(prompt());
+}
+
+// const result = arr.every(function(elem){
+//     if(!isNaN(elem)) {
+//         return true
+//     } else{
+//         return false
+//     }
+// })
+
+arr.forEach(function (elem) {
+    if (!isNaN(elem)) {
+        return true
+    } else {
+        return false
+    }
+})
