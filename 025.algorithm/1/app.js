2042d6f5be5fa90f5e23e12e3ad415cbc5469bf8
Vladislava <zaitsevavladislava13@gmail.com>
2023-02-21 11:15:05 +0300

    algorithm01

--- 025.algorithm/1/app.js
+++ 025.algorithm/1/app.js
@@ -1,28 +1,36 @@
-// Реализуйте функцию searchBinary, которая принимает 2 параметра: значение 3 и
+// 1. Реализуйте функцию searchBinary, которая принимает 2 параметра: значение 3 и
 // массив [1, 2, 3, 4, 5]. Функция возвращает индекс, где значение встречается в
 // массиве, если найдено. Если значение не найдено, возвращается сообщение об
-// ошибке
+// ошибке. 
 
-const n = 3;
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let count = 0;
+const num = 9;
 
-function searchBinary(arr_, n_) {
+
+function searchBinary(arr_, num_) {
     let start = 0;
     let end = arr_.length;
+    let count = 0;
 
     for (let i = start; i < end; i++) {
+
         let middle = Math.round((start + end) / 2);
         count++
-        if (arr_[middle] === n) {
-            console.log('Вы угадали', middle);
-            break
-        } else if (n_ > arr_[middle]) {
+
+        if (arr_[middle] == num_) {
+            console.log(`угадал! ваш индекс: ${middle}`);
+            break;
+        } else if (num_ > arr_[middle]) {
             start = middle;
-        }
-        else if (n_ < arr_[middle]) {
+        } else if (num_ < arr_[middle]) {
             end = middle;
         }
+
     }
+
+    console.log(count);
+
 }
-searchBinary(arr, n);
+
+
+searchBinary(arr, num)
