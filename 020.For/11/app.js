c7fad1bda48375228cff672fd132e01eb8f8bf84
Vladislava <zaitsevavladislava13@gmail.com>
2023-03-04 21:44:54 +0300

    For014-018

--- 020.For/11/app.js
+++ 020.For/11/app.js
@@ -1,2 +1,13 @@
 // У вас есть массив из строк, чисел и иных примитивов. Необходимо создать новый
 // массив, заполненный только числами первоначального массива
+
+const arr = ['q', 'w', 'e', 'r', 't', 1, 2, 3, 4, 5, false, true, NaN, null, undefined];
+let newArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(typeof (arr[i]));
+    if (typeof (arr[i]) === 'number') {
+        newArr.push(arr[i])
+    }
+}
+console.log(newArr);
