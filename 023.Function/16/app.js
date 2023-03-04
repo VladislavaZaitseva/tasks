6a5a14f13b99bf7331d92c89465ea87a61c6c9a0
Vladislava <zaitsevavladislava13@gmail.com>
2023-03-04 22:07:47 +0300

    Function016

--- 023.Function/16/app.js
+++ 023.Function/16/app.js
@@ -5,3 +5,40 @@
 // проверки – true, то вызывать новую функцию, возвращающую массив из всех
 // четных чисел, возведенных в квадрат
 
+const n = 5;
+
+function doArr(n_) {
+    let arr = [];
+    for (let i = 0; i < n_; i++) {
+        let prom = prompt('element of array');
+        arr.push(+prom);
+    }
+    return arr;
+}
+
+function isValid(arr_) {
+    const check = arr_.every(function (el) {
+        if (typeof (el) == 'number') {
+            return true;
+        } else return false;
+    })
+    return check;
+}
+
+function newArr(arr_) {
+    const booly = isValid(arr_1);
+
+    if (booly == true) {
+        let newArray = [];
+        for (let i = 0; i < arr_.length; i++) {
+            if (arr_[i] % 2 == 0) {
+                newArray.push(arr_[i] ** 2)
+            }
+        }
+        return newArray;
+    } else return 'error'
+}
+
+const arr_1 = doArr(n);
+const res = newArr(arr_1);
+console.log(res);
