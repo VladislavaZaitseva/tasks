846f715ec018b03e24f89d5bda253138970a9997
Vladislava <zaitsevavladislava13@gmail.com>
2023-03-04 22:10:17 +0300

    this016

--- 024.this/16/app.js
+++ 024.this/16/app.js
@@ -1,1 +1,14 @@
 // *Вычислите сумму массива чисел статичного массива используя рекурсию
+
+const arr = [1, 2, 3, 4, 5];
+
+console.log(arr.length);
+console.log(arr.length - 1);
+
+function sum(arr_, number) {
+    if (number == 0) return arr_[number];
+    return arr_[number] + sum(arr_, number - 1);
+}
+
+const result = sum(arr, arr.length - 1)
+console.log(result);
